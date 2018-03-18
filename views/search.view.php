14c01e4743fc0220c8a3aaadad2b57182346d44c
dkhansemand <dkhansemand@gmail.com>
2018-03-18 21:17:22 +0100

    minor fixes :wrench:

--- views/search.view.php
+++ views/search.view.php
@@ -5,6 +5,9 @@
     <h2>Søgning</h2>
     <section id="searchView">
     <?php
+    if(sizeof($results[0]) > 0 || sizeof($results[1]) > 0)
+    {
+
         foreach($results as $result)
         {
             foreach($result as $data)
@@ -17,7 +20,7 @@
                         <p><?=$data->newsTitle?></p>
                         <p><?=ucwords(strftime('%d. %B - %Y', strtotime($data->newsStartDate)))?></p>
                         <p>
-                        <?=(strlen($data->newsContent) > 200) ? substr($data->newsContent, 0, 200) . ' ...' : htmlspecialchars_decode($data->newsContent) ?>
+                        <?=(strlen($data->newsContent) > 100) ? substr($data->newsContent, 0, 100) . ' ...' : htmlspecialchars_decode($data->newsContent) ?>
                         </p>
                         <a href="<?=Router::Link('/Nyhed/'.str_replace(' ', '-', $data->newsTitle).'-'.$data->newsId)?>" class="btn-accent">Læs mere</a>
                     </article>
@@ -31,7 +34,9 @@
                         <img src="<?=Router::$BASE?>assets/media/<?=$data->filename?>" alt="">
                         <p><?=$data->eventTitle?></p>
                         <p><?=ucwords(strftime('%d. %B - %Y', strtotime($data->eventStartDate)))?></p>
-                        <p><?=htmlspecialchars_decode($data->eventDescription)?></p>
+                        <p>
+                            <?=(strlen($data->eventDescription) > 100) ? substr($data->eventDescription, 0, 100) . ' ...' : htmlspecialchars_decode($data->eventDescription) ?>
+                        </p>
                         <?php
                             if(isset($_SESSION['global']) && !empty($_SESSION['global']))
                             {
@@ -43,7 +48,7 @@
                         <?php
                                     }else{
                         ?>
-                                        <a href="<?=Router::Link('/Arrangementer/Tilmeld/'.$data->eventsId)?>" class="btn btn-event">Tilmeld</a>
+                                        <a href="<?=Router::Link('/Arrangementer')?>" class="btn btn-event">Læs mere</a>
                         <?php
                                     }
                         ?>
@@ -55,9 +60,13 @@
                 }
             }
         }
+    }
+    else
+    {
+?>
+    <h4>Din søgning returnerede ingen nyheder eller arrangemnter prøv at omformulere søgningen</h4>
+<?php
+    }
     ?>
     </section> 
-    <pre>
-        <?php //var_dump($results); ?>
-    </pre>
 </div>
